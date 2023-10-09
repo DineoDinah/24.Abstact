f6f2da516c55ee00ac45430bdd0444afbb7d8750
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-09 23:07:41 +0200

    Add project files.

---
+++ 24.Abstact/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vehicle car1 = new vehicle(); //this is not possible as parent class is abstract! to increase security
+            Mazda car2 = new Mazda();
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+        }
+        abstract class vehicle
+        {
+            public int wheels =4;
+        }
+        class Mazda : vehicle
+        {
+
+            public int sparewheels = 1;
+
+        }
+
+    }
+}
